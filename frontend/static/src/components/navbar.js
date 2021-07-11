5cd9c862cb9f7a2c101e9561a2001299fe892d90
katefranks <kmfranks9@gmail.com>
2021-07-11 16:39:25 -0400

    conditional rendering for authentication: navbar

--- frontend/static/src/components/navbar.js
+++ frontend/static/src/components/navbar.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Cookies from 'js-cookie';
 
 class Navigation extends Component {
   render() {
@@ -10,13 +11,15 @@ class Navigation extends Component {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto align-items-baseline">
-            <NavLink to='/' className="mr-2">Home</NavLink>
-            <NavLink to='/profile' className="mr-2">Profile</NavLink>
-            <NavLink to='/records' className="mr-2">Records</NavLink>
-            <NavLink to='/addRecord' className="mr-2">Add Record</NavLink>
-            <NavLink to='/login' className="mr-2">Login</NavLink>
-            <NavLink to='/registration' className="mr-2">Register</NavLink>
+            {!!Cookies.get('Authorization') && <NavLink to='/' className="mr-2">Home</NavLink> }
+            {!!Cookies.get('Authorization') && <NavLink to='/profile' className="mr-2">Profile</NavLink> }
+            {!!Cookies.get('Authorization') && <NavLink to='/records' className="mr-2">Records</NavLink> }
+            {!!Cookies.get('Authorization') && <NavLink to='/addRecord' className="mr-2">Add Record</NavLink> }
+            {!Cookies.get('Authorization') && <NavLink to='/login' className="mr-2">Login</NavLink> }
+            {!Cookies.get('Authorization') && <NavLink to='/registration' className="mr-2">Register</NavLink>}
+            {!!Cookies.get('Authorization') &&
             <button className="btn" onClick={() => this.props.handleLogout()}>Logout</button>
+            }
           </Nav>
         </Navbar.Collapse>
       </Navbar>
@@ -25,3 +28,6 @@ class Navigation extends Component {
 }
 
 export default Navigation;
+
+
+//
