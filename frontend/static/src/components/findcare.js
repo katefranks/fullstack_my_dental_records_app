84458cc919d9053c74cee8c74f26d1bd6a675f35
katefranks <kmfranks9@gmail.com>
2021-07-12 20:15:03 -0400

    integrated Places Autocomplete w/ Google Maps

--- frontend/static/src/components/findcare.js
+++ frontend/static/src/components/findcare.js
@@ -1,49 +1,101 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
+import PlacesAutocomplete, {geocodeByAddress,  getLatLng,} from 'react-places-autocomplete';
 // import { Loader } from "@googlemaps/js-api-loader"
 
 export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
 
-    mapCenter: {
-      lat: 34.852619,
-      lng: -82.394010,
-    },
-  };
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
 
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      })
+      mapCenter: {
+        lat: 34.852619,
+        lng: -82.394010,
+      }
     }
   };
 
+  handleChange = address => {
+      this.setState({ address });
+    };
+
+    handleSelect = address => {
+      geocodeByAddress(address)
+        .then(results => getLatLng(results[0]))
+        .then(latLng => {
+          console.log('Success', latLng)
+          this.setState({ address });
+          this.setState({ mapCenter: latLng });
+        })
+        .catch(error => console.error('Error', error));
+    };
+
   render() {
     return (
-      <Map google={this.props.google}
-        initialCenter={{
-          lat: this.state.mapCenter.lat,
-          lng: this.state.mapCenter.lng,
-        }}
+      <div id="googleMap">
+        <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
       >
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+        <Map google={this.props.google}
+          initialCenter={{
+            lat: this.state.mapCenter.lat,
+            lng: this.state.mapCenter.lng,
+          }}
+          center={{
+            lat: this.state.mapCenter.lat,
+            lng: this.state.mapCenter.lng,
+          }}
+        >
+        <Marker
+          position={{
+            lat: this.state.mapCenter.lat,
+            lng: this.state.mapCenter.lng,
+          }}
+        />
+        </Map>
+      </div>
 
-
-      </Map>
     )
   }
 }
